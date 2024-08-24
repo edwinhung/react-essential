52a5d6bcdd0a377e8455b85bff6468626606e6a4
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-08-24 15:26:08 -0700

    fill in the rest of core components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import reactImg from "./assets/react-core-concepts.png";
+import componentImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
 
 const descriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -21,11 +23,30 @@ function Header() {
   );
 }
 
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>

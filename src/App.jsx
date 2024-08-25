2659c501199e3dffc3604064506787ae5cf5f1d1
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-08-25 10:46:21 -0700

    add buttons for examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,7 @@
-import reactImg from "./assets/react-core-concepts.png";
-import componentImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data.js";
-
-const descriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = descriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} Fundamental React concepts you will need for almost any
-        app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
@@ -46,6 +16,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Component</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
         <h2>Time to get started!</h2>
       </main>

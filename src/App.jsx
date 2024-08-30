634ca64177742378188dba282fabf298572504fc
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-08-30 16:21:41 -0700

    refactor components to make App clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,16 @@
 import { useState, userState } from "react";
 
-import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
-import { EXAMPLES } from "./data.js";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
 
 function App() {
-  const [topic, setTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    setTopic(selectedButton);
-    // setTopic is scheduled to execute after App finished re-rendering
-    // console.log(topic);
-  }
-
   return (
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((item) => (
-              <CoreConcept key={item.title} {...item} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={topic === "components"}
-              onSelect={() => handleSelect("components")}
-            >
-              Component
-            </TabButton>
-            <TabButton
-              isSelected={topic === "jsx"}
-              onSelect={() => handleSelect("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={topic === "props"}
-              onSelect={() => handleSelect("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={topic === "state"}
-              onSelect={() => handleSelect("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-
-          {!topic && <p>Please select a topic</p>}
-          {topic && (
-            <div id="tab-content">
-              <h3>{EXAMPLES[topic].title}</h3>
-              <p>{EXAMPLES[topic].description}</p>
-              <pre>
-                <code>{EXAMPLES[topic].code}</code>
-              </pre>
-            </div>
-          )}
-        </section>
+        <CoreConcepts />
+        <Examples />
         <h2>Time to get started!</h2>
       </main>
     </>
